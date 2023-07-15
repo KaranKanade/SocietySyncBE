82fed38c76cd7935235a9ba559b9ba21a9198070
Karan Kanade <karan.kanade@gelato.com>
2023-07-15 21:20:23 +0530

    Initial commit

---
+++ backend/src/main/java/com/societysync/backend/SocietysyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.societysync.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietysyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietysyncBackendApplication.class, args);
+	}
+
+}
